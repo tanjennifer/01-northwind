261d7638941be26526135223667badc5b9f29e21
Jennifer <jennifertan1994@gmail.com>
2023-11-07 09:04:04 -0500

    sql practice

---
+++ answers.sql
@@ -0,0 +1,54 @@
+-- 1. select * from customers;
+-- 2. select distinct country from customers;
+-- 3. SELECT * FROM customers WHERE customer_id LIKE 'BL%';
+-- 4. SELECT * FROM orders limit 100
+-- 5. SELECT * FROM customers WHERE postal_code IN ('1010', '3012', '12209', '05023');
+-- 6. SELECT * FROM orders WHERE ship_region IS NOT NULL;
+-- 7. SELECT * FROM customers ORDER BY country, city;
+-- 8 INSERT INTO customers (customer_id, company_name, contact_name, contact_title, address, city, region, postal_code, country, phone, fax)
+-- VALUES ('Cu123', 'The Cat Company', 'Billy Koppel', 'Cat Whisperer', '123 Oscar Street', 'Ann Arbor', 'MI', '10001', 'USA', '1234567890', 'null');
+-- 9. UPDATE orders
+-- SET ship_region = "EuroZone"
+-- WHERE ship_country = "France";
+-- 10. DELETE FROM order_details WHERE quantity = 1;
+-- 11. SELECT
+--   AVG(quantity),
+--   MAX(quantity),
+--   MIN(quantity)
+-- FROM order_details;
+-- 12. SELECT
+--   AVG(quantity),
+--   MAX(quantity),
+--   MIN(quantity)
+-- FROM order_details
+-- GROUP BY order_id;
+-- 13. SELECT customer_id FROM orders WHERE order_id = 10290;
+
+-- 14. inner join: only orders w matching cust details; matching rows from both side
+-- SELECT orders.order_id, customers.customer_id, customers.contact_name
+-- FROM orders
+-- INNER JOIN customers ON orders.customer_id = customers.customer_id;
+
+-- left join: retrieved cust details for matching orders; everything from left and matching from right
+-- SELECT orders.order_id, customers.customer_id, customers.contact_name
+-- FROM orders
+-- LEFT JOIN customers ON orders.customer_id = customers.customer_id;
+
+-- right join: returns rows from cust table and matching from orders table
+-- SELECT orders.order_id, customers.customer_id, customers.contact_name
+-- FROM orders
+-- RIGHT JOIN customers ON orders.customer_id = customers.customer_id;
+
+-- 15. SELECT orders.ship_city, orders.ship_country
+-- FROM orders
+-- JOIN employees ON orders.employee_id = employees.employee_id
+-- WHERE employees.city = 'London';
+-- 16.
+-- SELECT DISTINCT ship_name
+-- FROM orders
+-- JOIN order_details ON orders.order_id = order_details.order_id
+-- JOIN products ON order_details.product_id = products.product_id
+-- WHERE products.discontinued = 1;
+-- 17. SELECT first_name FROM employees WHERE reports_to IS NULL;
+-- 18. SELECT first_name FROM employees WHERE reports_to= 2;
+-- 19. CREATE INDEX customer_id ON customers (customer_id);
